085605cfab20bf18c281fc3878573f24fc8d6e02
Dan <dpedersen84@gmail.com>
2019-07-01 15:40:27 -0500

    getters/setters

--- src/main/java/com/dp/flooringmastery/models/Product.java
+++ src/main/java/com/dp/flooringmastery/models/Product.java
@@ -3,16 +3,43 @@ package com.dp.flooringmastery.models;
 import java.math.BigDecimal;
 
 public class Product {
-    private final String name;
-    private final BigDecimal costPerSqFt;
-    private final BigDecimal laborCostPerSqFt;
+    private String name;
+    private BigDecimal costPerSqFt;
+    private BigDecimal laborCostPerSqFt;
 
-    private Product(String name, BigDecimal costPerSqFt, BigDecimal laborCostPerSqFt) {
+    public Product() {
+    }
+    
+    public Product(String name, BigDecimal costPerSqFt, BigDecimal laborCostPerSqFt) {
         this.name = name;
         this.costPerSqFt = costPerSqFt;
         this.laborCostPerSqFt = laborCostPerSqFt;
     }
 
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getCostPerSqFt() {
+        return costPerSqFt;
+    }
+
+    public void setCostPerSqFt(BigDecimal costPerSqFt) {
+        this.costPerSqFt = costPerSqFt;
+    }
+
+    public BigDecimal getLaborCostPerSqFt() {
+        return laborCostPerSqFt;
+    }
+
+    public void setLaborCostPerSqFt(BigDecimal laborCostPerSqFt) {
+        this.laborCostPerSqFt = laborCostPerSqFt;
+    }
+    
     public BigDecimal calculateTotalCostPerSqFoot(BigDecimal area) {
         return this.costPerSqFt.multiply(area);
     }
