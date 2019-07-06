945a2d373b88743cf148ded40877d4ba67ef75d5
Dan <dpedersen84@gmail.com>
2019-07-06 13:59:46 -0500

    new exception

---
+++ src/main/java/com/dp/flooringmastery/service/InvalidOrderNumberException.java
@@ -0,0 +1,12 @@
+package com.dp.flooringmastery.service;
+
+public class InvalidOrderNumberException extends Exception {
+
+    InvalidOrderNumberException(String message) {
+        super(message);
+    }
+
+    InvalidOrderNumberException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
